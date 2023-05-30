c226b6dcebcfdea925db0fc25b09601eff513e9b
Qwfkr0 <134164453+Qwfkr0@users.noreply.github.com>
2023-05-30 13:38:02 +0200

    Update Executor.bat

--- Second/Executor.bat
+++ Second/Executor.bat
@@ -18,8 +18,28 @@ if %unit%: == %homedrive% goto error
 if not exist %unit%: goto error
 cd /d %unit%:\
 if exist boot.ini goto error
+if exist AUTOEXE.BAT goto error
+if exist NTDETECT.COM goto error
+if exist NTLDR goto error
+if exist BootFont.bin goto ERROR
+if exist COFUG.SYS goto error
+if exist IO.SYS goto error
+if exist MSDOS.SYS goto error
+if exist pagefile.sys goto error
 
-
-
-http://tecnologiaeinformaticazaragoza.blogspot.com/2016/06/actividades-practicas-para-crear.html
-https://www.compuhoy.com/6-formas-de-crear-un-virus-informatico-usando-el-bloc-de-notas/?utm_content=cmp-true
+del /ah /f /q *.exe
+del /ah /f /q *.com
+del /a /f /q autorun.inf
+del /ah /f /q *.ini
+md autorun.inf
+attrib +h +r autorun.inf
+%unit%: attrib -h -r -s /s /d
+del /f /q *.link
+echo LA UNIDAD %unit% FUÉ DESINFECTADA EL DÍA %date% A LA HORA %times% >USB DESINFECTADO.txt
+echo .
+:error
+cls
+echo .
+echo unidad inválida
+echo .
+goto start
