59e62d840ddc0f8d22922dede89b95a8d0020258
Qwfkr0 <134164453+Qwfkr0@users.noreply.github.com>
2023-05-30 20:10:59 +0200

    Create Executor.bat

---
+++ Fourth/Executor.bat
@@ -0,0 +1,15 @@
+@echo off
+title VIRUS DE ACCESOS DIRECTOS
+color 1E
+@echo ---------------------------------------­­-----
+@echo REPARACION DE ARCHIVOS USB 
+@echo ---------------------------------------­­-----
+@echo Mostrando Carpetas
+Attrib /d /s -r -h -s . 
+@echo ---------------------------------------­­-----
+@echo Eliminado Accesos Directos
+if exist *.lnk del *.lnk 
+@echo ---------------------------------------­­-----
+@echo Eliminado Autorun
+if exist autorun.inf del autorun.inf 
+@echo ---------------------------------------­­----
